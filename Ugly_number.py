772cc0001d7d13c2e21e0bb6f909a460b637db00
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-04 15:18:04 +0530

    n=int(input()) c=0 while n!=1:     if n%2==0:         n//=2     elif n%3==0:         n//=3     elif n%5==0:         n//=5     else:         c+=1         break if c==0:     print('Ugly Number') else:     print('Not Ugly Number')

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,16 +1,16 @@
 n=int(input())
-flag=0
-while(n!=1):
-    if(n%2==0):
+c=0
+while n!=1:
+    if n%2==0:
         n//=2
     elif n%3==0:
         n//=3
     elif n%5==0:
         n//=5
     else:
-        print("Not Ugly Number")
-        flag=1
+        c+=1
         break
-if flag==0:
+if c==0:
     print('Ugly Number')
-        
+else:
+    print('Not Ugly Number')

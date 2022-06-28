f4bd40ab267b810065ab0b381ec8017ad24033e2
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-28 22:33:00 +0530

    n=int(input()) a=list(map(int,input().split())) for i in a:     if i%2==0:         print(i,end=' ') for i in a:     if i%2!=0:         print(i,end=' ')

--- even_elements_followed_by_odd.py
+++ even_elements_followed_by_odd.py
@@ -3,6 +3,6 @@ a=list(map(int,input().split()))
 for i in a:
     if i%2==0:
         print(i,end=' ')
-for j in a:
-    if j%2!=0:
-        print(j,end=' ')
+for i in a:
+    if i%2!=0:
+        print(i,end=' ')

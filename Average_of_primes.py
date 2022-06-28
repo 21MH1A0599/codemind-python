82142fe9abe3855e03f30f47ee2bdf3da9ccff1d
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-28 21:48:49 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) a=list(map(int,input().split())) s,c=0,0 for i in a:     if prime(i):         s+=i         c+=1 avg=s/c print("%.2f"%avg)

--- Average_of_primes.py
+++ Average_of_primes.py
@@ -8,12 +8,10 @@ def prime(n):
         return 1
 n=int(input())
 a=list(map(int,input().split()))
-s=0
-c=0
+s,c=0,0
 for i in a:
     if prime(i):
         s+=i
         c+=1
 avg=s/c
 print("%.2f"%avg)
-            

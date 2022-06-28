62efef7d5d51b81f7083f9b6b1e61a560a342e3d
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-28 16:54:50 +0530

    n=int(input()) sum=0 while True:     r=n%10     sum+=r     n//=10     if n==0 and sum>9:         n=sum         sum=0     elif n==0 and sum<=9:         print(sum)         break

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,12 @@
 n=int(input())
 sum=0
-while 1:
+while True:
     r=n%10
     sum+=r
     n//=10
     if n==0 and sum>9:
         n=sum
         sum=0
-    elif n==0 and sum<9:
+    elif n==0 and sum<=9:
         print(sum)
         break

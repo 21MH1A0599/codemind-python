fdd0ee51a03335c0b70e512454d0d902652077e6
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-29 22:14:36 +0530

    a=input() a=a.lower() a=a.split() v='aeiou' r=[] c1=0 for i in a:     c=0     for j in i:         if j in v:             c+=1     r.append(c) for i in r:     if i==min(r):         c1+=1 print(c1)

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,10 +1,10 @@
-n=input()
-n=n.lower()
-n=n.split()
-r=[]
-f=0
+a=input()
+a=a.lower()
+a=a.split()
 v='aeiou'
-for i in n:
+r=[]
+c1=0
+for i in a:
     c=0
     for j in i:
         if j in v:
@@ -12,5 +12,5 @@ for i in n:
     r.append(c)
 for i in r:
     if i==min(r):
-        f+=1
-print(f)
+        c1+=1
+print(c1)

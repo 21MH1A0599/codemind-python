f22ac8acc5fb8213ebaef71172985aa1d59bb03b
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-30 23:01:48 +0530

    def reverse(n):     rev=0     while n:         r=n%10         rev=rev*10+r         n//=10     return rev def square(n):     c=n*n     return c n=int(input()) a=square(n) b=reverse(n) c=square(b) if a==reverse(c):     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,18 +1,18 @@
-def sqr(n):
-    s=n*n
-    return s
 def reverse(n):
     rev=0
     while n:
         r=n%10
         rev=rev*10+r
         n//=10
-    return rev    
+    return rev
+def square(n):
+    c=n*n
+    return c
 n=int(input())
-a=sqr(n)
+a=square(n)
 b=reverse(n)
-c=sqr(b)
-if(a==reverse(c)):
+c=square(b)
+if a==reverse(c):
     print('True')
 else:
     print('False')

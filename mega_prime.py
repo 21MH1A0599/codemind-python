707f3865bb528c9674f9a20463a27c97b0ed3885
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-30 23:09:33 +0530

    n=int(input()) for i in range(2,n):     if n%i==0:         print('Not Mega Prime')         break else:     d,p=0,0     while n:         r=n%10         n//=10         d+=1         if r==2 or r==3 or r==5 or r==7:             p+=1 if d==p:     print('Mega Prime') else:     print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -4,16 +4,14 @@ for i in range(2,n):
         print('Not Mega Prime')
         break
 else:
-    d=0
-    pd=0
+    d,p=0,0
     while n:
         r=n%10
-        n=n//10
+        n//=10
         d+=1
         if r==2 or r==3 or r==5 or r==7:
-            pd+=1
-
-if d==pd:
+            p+=1
+if d==p:
     print('Mega Prime')
 else:
     print('Not Mega Prime')

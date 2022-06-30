9101e9d26133e466e02abdc16a2971827fe568e7
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-30 22:49:58 +0530

    n=input() a=list(n) a=set(a) if len(n)==len(a):     print('Unique Number') else:     print('Not Unique Number')

--- Unique_number.py
+++ Unique_number.py
@@ -1,27 +1,7 @@
-n=int(input())
-a,b,c,d,e,f,g,h,i=0,0,0,0,0,0,0,0,0
-while n:
-    r=n%10
-    n//=10
-    if r==1:
-        a+=1
-    elif r==2:
-        b+=1
-    elif r==3:
-        c+=1
-    elif r==4:
-        d+=1
-    elif r==5:
-        e+=1    
-    elif r==6:
-        f+=1
-    elif r==7:
-        g+=1    
-    elif r==8:
-        h+=1
-    elif r==9:
-        i+=1
-if a>1 or b>1 or c>1 or d>1 or e>1 or f>1 or g>1 or h>1 or i>1:
-    print("Not Unique Number")
+n=input()
+a=list(n)
+a=set(a)
+if len(n)==len(a):
+    print('Unique Number')
 else:
-    print("Unique Number")
+    print('Not Unique Number')

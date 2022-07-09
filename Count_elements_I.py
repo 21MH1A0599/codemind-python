b2bf25cb0b5ae0fb688c6065bd9914fefbf65ce1
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-09 17:56:18 +0530

    x,y=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) r=[] for i in a:     for j in b:         if i==j:             if i not in r:                 r.append(i) print(len(r))

---
+++ Count_elements_I.py
@@ -0,0 +1,10 @@
+x,y=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+r=[]
+for i in a:
+    for j in b:
+        if i==j:
+            if i not in r:
+                r.append(i)
+print(len(r))

e804e96e04d5d20dd4f05318caf609b2ea7eea8e
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-09 21:35:46 +0530

    n=int(input()) a=list(map(int,input().split())) r=[] c=0 for i in a:     if i==a.count(i):         if i not in r:             r.append(i)             c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,10 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=0
 r=[]
+c=0
 for i in a:
     if i==a.count(i):
         if i not in r:
             r.append(i)
             c+=1
 print(c)
+    

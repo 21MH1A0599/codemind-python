0889fda91d916d972fff3c8eefed65c489276eae
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-09 17:00:09 +0530

    n=int(input()) a=list(map(int,input().split())) x,y=map(int,input().split()) r=[] for i in a:     if i>=x and i<=y:         f=1     else:         r.append(i) if len(r)==0:     print('-1') else:     print(min(r))

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+x,y=map(int,input().split())
+r=[]
+for i in a:
+    if i>=x and i<=y:
+        f=1
+    else:
+        r.append(i)
+if len(r)==0:
+    print('-1')
+else:
+    print(min(r))

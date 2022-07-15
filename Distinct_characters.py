85e2302b4244c0b2614fb7ca9aff528d67b2f7fc
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-15 18:10:00 +0530

    a=input() a=a.lower() a=a.replace(" ","") a=sorted(a) r=[] for i in a:     if i not in r:         r.append(i) for i in r:     if i!=' ':         print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,10 @@
-n=input()
-n=n.lower()
-n=n.replace(" ","")
-n=sorted(n)
+a=input()
+a=a.lower()
+a=a.replace(" ","")
+a=sorted(a)
 r=[]
-for i in n:
-    if n.count(i)==1:
+for i in a:
+    if i not in r:
         r.append(i)
 for i in r:
     if i!=' ':

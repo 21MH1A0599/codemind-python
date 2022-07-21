7a77189f43ab1ebc3698bec813be99e253b666b1
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-21 18:14:45 +0530

    n=int(input()) a=list(map(int,input().split())) r=[] for i in a:     if i>=0:         i=str(i)         k=len(i)         r.append(k)     else:         i=str(i)         k=len(i)         r.append(k-1) print(*r)

--- count_digits.py
+++ count_digits.py
@@ -5,9 +5,9 @@ for i in a:
     if i>=0:
         i=str(i)
         k=len(i)
-        print(k,end=' ')
+        r.append(k)
     else:
         i=str(i)
-        k1=len(i)
-        print(k1-1,end=' ')
-        
+        k=len(i)
+        r.append(k-1)
+print(*r)

e14a542ba50d822bf1a27672c415eb61ff8b5bf2
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-09-27 23:08:45 +0530

    s=input().lower() s=s.split() v='aeiou' l=[] for i in s:    for j in v:        if i not in l:            if i.startswith(j):                l.append(i) print(len(l))

--- count_words.py
+++ count_words.py
@@ -1,10 +1,10 @@
-s=input()
-s=s.lower()
+s=input().lower()
 s=s.split()
 v='aeiou'
 l=[]
 for i in s:
-    for j in v:
-        if i.startswith(j):
-            l.append(i)
-print(len(l))l
+   for j in v:
+       if i not in l:
+           if i.startswith(j):
+               l.append(i)
+print(len(l))

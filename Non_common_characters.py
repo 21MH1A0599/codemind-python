5a37d1f580ab8e516fdfdefb90dace64114a414a
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-09-27 22:38:53 +0530

    s1=input().lower() s2=input().lower() l=[] for i in s1:     if i not in s2:         if i not in l:             l.append(i) for i in s2:     if i not in s1:         if i not in l:             l.append(i) l=str(l) l=l.replace(" ","") l=l.replace("[","") l=l.replace("]","") l=l.replace(",","") l=l.replace("'","") print(len(l))

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,11 +1,18 @@
 s1=input().lower()
 s2=input().lower()
-l=''
+l=[]
 for i in s1:
-    if i not in s2 and i not in l:
-        l+=i
+    if i not in s2:
+        if i not in l:
+            l.append(i)
 for i in s2:
-    if i not in s1 and i not in l:
-        l+=i
+    if i not in s1:
+        if i not in l:
+            l.append(i)
+l=str(l)
 l=l.replace(" ","")
+l=l.replace("[","")
+l=l.replace("]","")
+l=l.replace(",","")
+l=l.replace("'","")
 print(len(l))

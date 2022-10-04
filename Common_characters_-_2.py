91b78667897748969b9ebb8e7cd09ec07179a07c
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-10-04 19:18:42 +0530

    n=input().lower() n=n.split() l=[] c=0 a=n[0] for i in a:     c=0     for j in range(1,len(n)):         if i in n[j]:             c+=1     if c==len(n)-1:         l.append(i) print(len(l))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,13 @@
+n=input().lower()
+n=n.split()
+l=[]
+c=0
+a=n[0]
+for i in a:
+    c=0
+    for j in range(1,len(n)):
+        if i in n[j]:
+            c+=1
+    if c==len(n)-1:
+        l.append(i)
+print(len(l))

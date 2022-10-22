c7865f609cc779e3d230c74955c6fd88e59a278b
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-10-22 19:19:33 +0530

    s=input().lower() s=s.split() a=s[0] l=[] for i in a:     c=0     for j in range(1,len(s)):         if i in s[j]:             c+=1     if c==len(s)-1:         l.append(i) k="".join(l) if len(l)==0:     print("-1") else:     print(k)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,16 +1,16 @@
-n=input().lower()
-n=n.split()
-l=''
-c=0
-a=n[0]
+s=input().lower()
+s=s.split()
+a=s[0]
+l=[]
 for i in a:
     c=0
-    for j in range(1,len(n)):
-        if i in n[j]:
+    for j in range(1,len(s)):
+        if i in s[j]:
             c+=1
-    if c==len(n)-1:
-        l+=i
+    if c==len(s)-1:
+        l.append(i)
+k="".join(l)
 if len(l)==0:
     print("-1")
 else:
-    print(l)
+    print(k)
